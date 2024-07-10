2750e8955d70ed895deede61f27cf40a9845f4dc
Harshit Kandpal <111063866+harshitkandpal@users.noreply.github.com>
2024-07-10 22:11:40 +0530

    Update NewCampaign.jsx

--- phishing-dashboard/src/components/NewCampaign.jsx
+++ phishing-dashboard/src/components/NewCampaign.jsx
@@ -42,7 +42,7 @@ const NewCampaign = () => {
   const fetchData = async () => {
     try {
       setIsLoading(true);
-      const response = await fetch(`https://api.hunter.io/v2/domain-search?domain=${domain}&api_key=9615080049c281d286cb459e379ddb04be9ea7e4`);
+      const response = await fetch();
       const jsonData = await response.json();
       console.log('Fetched data:', jsonData);
       setData((prevData) => ({ ...prevData, ...jsonData }));
