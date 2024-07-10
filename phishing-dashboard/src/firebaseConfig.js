98b65e3ea2d953d10b6e3f472c5b2aef475a79ad
Harshit Kandpal <111063866+harshitkandpal@users.noreply.github.com>
2024-07-10 22:09:15 +0530

    Update firebaseConfig.js

--- phishing-dashboard/src/firebaseConfig.js
+++ phishing-dashboard/src/firebaseConfig.js
@@ -8,13 +8,7 @@ import { getFirestore, doc, updateDoc, getDoc} from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDIdO1sfPFLKz46kpWxTWSE39Gm0WHgQSE",
-  authDomain: "phishing-dashboard-dfb4e.firebaseapp.com",
-  projectId: "phishing-dashboard-dfb4e",
-  storageBucket: "phishing-dashboard-dfb4e.appspot.com",
-  messagingSenderId: "693259460131",
-  appId: "1:693259460131:web:fd6fefe2974ab7a9309b96",
-  measurementId: "G-MEHV5G841M"
+  
 };
 
 // Initialize Firebase
